96f5b3faab5e8e389765c8a3c0947a483232d2fc
Quan Nguyen Trung <quanxutk1@outlook.com>
2025-01-17 15:00:10 +0700

    Section II: start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,17 @@
-import 'package:flutter/material.dart';
-import 'gradient_container.dart';
-
-import 'book.dart';
-
-void main_test(List<String> args) {
-  // String name = 'Nguyen Trung Quan';
-  // print(name);
-
-  // Map<String, String> objects = {'username': 'quanxutk1', 'password': '123'};
-  // print(objects['username']);
-
-  var myBook = Book('Sach hoc tap', 2024);
-  print(myBook.getName);
-
-  myBook.setName = 'Sach khoa hoc';
-  print(myBook.getName);
-
-  print(Book.age);
-
-  var bookFunny = BookFunny('Tho va rua', 2025);
-  bookFunny.type = 'Funny';
-  print(bookFunny.type);
-  bookFunny.showHello();
-}
+import "package:flutter/material.dart";
+import "start_screen.dart";
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: GridientContainer(Colors.deepOrange, Colors.lightBlue),
+      body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.deepPurple, Colors.lightBlue],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+          child: const StartScreen()),
     ),
   ));
 }
