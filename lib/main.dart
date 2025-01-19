3ae11d142ea3667e6e87f5009e9b9e5be458c517
Quan Nguyen Trung <quanxutk1@yahoo.com>
2025-01-19 16:59:00 +0700

    Home

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 import 'gradient_container.dart';
 
-import 'book.dart';
+//import 'book.dart';
 
-void main_test(List<String> args) {
-  // String name = 'Nguyen Trung Quan';
-  // print(name);
+// void main_test(List<String> args) {
+//   // String name = 'Nguyen Trung Quan';
+//   // print(name);
 
-  // Map<String, String> objects = {'username': 'quanxutk1', 'password': '123'};
-  // print(objects['username']);
+//   // Map<String, String> objects = {'username': 'quanxutk1', 'password': '123'};
+//   // print(objects['username']);
 
-  var myBook = Book('Sach hoc tap', 2024);
-  print(myBook.getName);
+//   var myBook = Book('Sach hoc tap', 2024);
+//   print(myBook.getName);
 
-  myBook.setName = 'Sach khoa hoc';
-  print(myBook.getName);
+//   myBook.setName = 'Sach khoa hoc';
+//   print(myBook.getName);
 
-  print(Book.age);
+//   print(Book.age);
 
-  var bookFunny = BookFunny('Tho va rua', 2025);
-  bookFunny.type = 'Funny';
-  print(bookFunny.type);
-  bookFunny.showHello();
-}
+//   var bookFunny = BookFunny('Tho va rua', 2025);
+//   bookFunny.type = 'Funny';
+//   print(bookFunny.type);
+//   bookFunny.showHello();
+//}
 
 void main() {
   runApp(const MaterialApp(
